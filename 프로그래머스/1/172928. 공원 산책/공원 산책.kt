8ae887dd32b7c61d9b650cd1991909798c9aac87
AWKRID <137989290+AWKRID@users.noreply.github.com>
2024-05-02 10:32:40 +0900

    [level 1] Title: 공원 산책, Time: 7.05 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.kt
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.kt
@@ -1,36 +1,6 @@
 class Solution {
     fun solution(park: Array<String>, routes: Array<String>): IntArray {
-        var cy = 0
-        var cx = 0
-        for(y in park.indices){
-            for(x in park[0].indices){
-                if(park[y][x] == 'S') {
-                    cy = y
-                    cx = x
-                    break
-                }
-            }
-        }
-        for(route in routes) {
-            val routeInfo = route.split(" ")
-            var dy = cy
-            var dx = cx
-            for(i in 1..routeInfo[1].toInt()){
-                when(routeInfo[0]){
-                    "E" -> dx++
-                    "W" -> dx--
-                    "S" -> dy++
-                    "N" -> dy--
-                }
-                if(dy < 0 || dy >= park.size || dx < 0 || dx >= park[0].length||park[dy][dx] == 'X') {
-                    dy = cy
-                    dx = cx
-                    break
-                }
-            }
-            cy = dy
-            cx = dx
-        }
-        return intArrayOf(cy, cx)
+        var answer: IntArray = intArrayOf()
+        return answer
     }
 }
